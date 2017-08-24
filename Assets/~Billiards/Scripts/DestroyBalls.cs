1aa9fa751fcae187a816296dfd925104391b9be9
Unknown <wndtanaka@gmail.com>
2017-08-24 16:07:27 +1000

    Billiards

---
+++ Assets/~Billiards/Scripts/DestroyBalls.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBalls : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/~Billiards/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Billiards
+{
+    public class GameManager : MonoBehaviour
+    {
+        public Ball[] ball;
+
+        private Rigidbody rb;
+        public Cue cue;
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rb.IsSleeping())
+            {
+                cue.Activate();
+            }
+        }
+    }
+}
