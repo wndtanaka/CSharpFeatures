0849420d820859f7d09318c4c270eb791d76596b
Unknown <wndtanaka@gmail.com>
2017-08-31 15:45:05 +1000

    Tower Defense

---
+++ Assets/~TowerDefense/Scripts/AIAgent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if target is not nill
+            if (target != null)
+            {                
+                // set nav destination to target's position
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
