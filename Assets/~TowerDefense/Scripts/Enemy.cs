3c561f7ea5dfd6de912955945c270773caf4d14b
Unknown <wndtanaka@gmail.com>
2017-09-12 16:11:55 +1000

    bit

--- Assets/~TowerDefense/Scripts/Enemy.cs
+++ Assets/~TowerDefense/Scripts/Enemy.cs
@@ -7,17 +7,7 @@ namespace TowerDefense
     public class Enemy : MonoBehaviour
     {
         public float health = 100f; // enemy's health which starts at 100
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        
         public void DealDamage(float damage)
         {
             health -= damage;
--- Assets/~TowerDefense/Scripts/Towers/Tower.cs
+++ Assets/~TowerDefense/Scripts/Towers/Tower.cs
@@ -6,11 +6,59 @@ namespace TowerDefense
 {
     public class Tower : MonoBehaviour
     {
-
         public Cannon cannon; // Reference to cannon inside of tower
         public float attackRate = 0.25f; // Rate of attack in seconds
         public float attackRadius = 5f; // Distance of attack in world units
         private float attackTimer = 0f; // Timer to count up to attackRate
         private List<Enemy> enemies = new List<Enemy>(); // List of enemies within radius
+
+        void OnTriggerEnter(Collider col)
+        {
+            Enemy e = col.GetComponent<Enemy>();
+            if(e!= null)
+            {
+                enemies.Add(e);
+            }
+        }
+        void OnTriggerExit(Collider col)
+        {
+            Enemy e = col.GetComponent<Enemy>();
+            if (e != null) ;
+            {
+                enemies.Remove(e);
+            }
+        }
+        Enemy GetClosestEnemy()
+        {
+            Enemy closest = null;
+            float minDistance = float.MaxValue;
+            foreach (Enemy enemy in enemies)
+            {
+                float distance = Vector3.Distance(cannon.transform.position, enemy.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closest = enemy;
+                }
+            }
+            return closest;
+        }
+        void Attack()
+        {
+            Enemy closest =  GetClosestEnemy();
+            if(closest!= null)
+            {
+                cannon.Fire(closest);
+            }
+        }
+        void Update()
+        {
+            attackTimer *= Time.deltaTime;
+            if (attackTimer >= attackRate)
+            {
+                Attack();
+                attackTimer = 0;
+            }
+        }
     }
 }
