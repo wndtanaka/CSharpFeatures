f44e91eda1359211228bdc9049c2238553ddfc40
Unknown <wndtanaka@gmail.com>
2017-09-21 11:58:11 +1000

    blah

---
+++ Assets/~AHundredBalls/Scripts/BallMaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMaterial : MonoBehaviour
+{
+    public Material[] mat;
+
+    Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomIndex = Random.Range(0, mat.Length);
+    }
+}
---
+++ Assets/~AHundredBalls/Scripts/ScoreBoard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    Text scoreBoard;
+
+    // Use this for initialization
+    void Awake()
+    {
+        scoreBoard = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayScore();
+    }
+    void DisplayScore()
+    {
+        scoreBoard.text = "Score: " + score;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Bucket")
+        {
+            score++;
+            Debug.Log(score);
+        }
+    }
+}
